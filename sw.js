09fbd1158563b93105eeef5e028f830ebdda0a31
Chappy-git <mari.lisa.yuto@gmail.com>
2025-08-11 11:31:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='amida-pages-v8';
+const CACHE_NAME='amida-pages-v9';
 const CORE=['./','./index.html','./manifest.json','./icons/icon-64.png','./icons/icon-128.png','./icons/icon-192.png','./icons/icon-256.png','./icons/icon-384.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)));self.skipWaiting());});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>res).catch(()=>{if(e.request.mode==='navigate'||(e.request.headers.get('accept')||'').includes('text/html')){return caches.match('./index.html');}})));});
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>res).catch(()=>{ if(e.request.mode==='navigate'||(e.request.headers.get('accept')||'').includes('text/html')){ return caches.match('./index.html'); } }))); });
